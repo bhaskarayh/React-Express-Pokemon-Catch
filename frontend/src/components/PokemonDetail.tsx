6d9aa13ee960c5c7296f5afa1cdd702676825cb2
bhaskarayh <bhaskarayh@gmail.com>
2024-08-05 21:21:27 +0700

    Update add nickname swalfire

--- frontend/src/components/PokemonDetail.tsx
+++ frontend/src/components/PokemonDetail.tsx
@@ -7,6 +7,8 @@ import {
 } from "../api";
 import { showAlert } from "./Alert";
 import { usePokemon } from "../context/PokemonContext";
+import withReactContent from "sweetalert2-react-content";
+import Swal from "sweetalert2";
 
 const PokemonDetail: React.FC = () => {
   const { name } = useParams<{ name: string }>();
@@ -14,15 +16,33 @@ const PokemonDetail: React.FC = () => {
   const [nickname, setNickname] = React.useState<string>("");
   const [catchSuccess, setCatchSuccess] = React.useState<boolean | null>(null);
   const [loading, setLoading] = React.useState<boolean>(false);
+  const [loadingCard, setLoadingCard] = React.useState<boolean>(false);
   const { caughtPokemons, addPokemon } = usePokemon();
 
   React.useEffect(() => {
     const loadPokemonDetails = async () => {
-      const details = await fetchPokemonDetails(name!);
-      setPokemon(details);
+      setLoadingCard(true);
+      try {
+        // await new Promise((resolve) => setTimeout(resolve, 3000));
+
+        const details = await fetchPokemonDetails(name!);
+        setPokemon(details);
+      } catch (error) {
+        console.error("Error fetching Pokemon details:", error);
+        await showAlert({
+          title: "Error",
+          text: "Failed to load Pokémon details.",
+          icon: "error",
+          confirmButtonText: "OK",
+        });
+      } finally {
+        setLoadingCard(false);
+      }
     };
     loadPokemonDetails();
   }, [name]);
+
+  const SwalWithReactContent = withReactContent(Swal);
 
   const handleCatch = async () => {
     setLoading(true);
@@ -31,16 +51,42 @@ const PokemonDetail: React.FC = () => {
     const success = Math.random() * 100 < probability;
     setCatchSuccess(success);
     if (success) {
-      const newName = await renamePokemon(name!, 0);
-      console.log("Caught Pokemon:", newName);
-
-      addPokemon(newName.newName, name, 0);
-      await showAlert({
-        title: "Success",
-        text: `Success catch ${name?.toLocaleUpperCase()} into pokeball`,
-        icon: "success",
-        confirmButtonText: "OK",
+      const { value: nickname } = await SwalWithReactContent.fire({
+        title: "Success!",
+        text: `You caught ${name?.toLocaleUpperCase()}! Enter a nickname for your new Pokémon.`,
+        input: "text",
+        inputLabel: "Nickname",
+        inputPlaceholder: "Enter nickname",
+        showCancelButton: true,
+        confirmButtonText: "Save",
+        cancelButtonText: "Cancel",
+        inputValidator: (value) => {
+          if (!value) {
+            return "You need to enter a nickname!";
+          }
+          return null;
+        },
       });
+
+      if (nickname) {
+        const newName = await renamePokemon(nickname!, 0);
+        console.log("Caught Pokémon:", newName);
+
+        addPokemon(newName.newName, name!, 0);
+        await showAlert({
+          title: "Success",
+          text: `Successfully caught ${name?.toLocaleUpperCase()} and named it ${nickname}!`,
+          icon: "success",
+          confirmButtonText: "OK",
+        });
+      } else {
+        await showAlert({
+          title: "Catch Cancelled",
+          text: `You chose not to catch ${name?.toLocaleUpperCase()}.`,
+          icon: "info",
+          confirmButtonText: "OK",
+        });
+      }
     } else {
       await showAlert({
         title: "Failed",
@@ -58,6 +104,61 @@ const PokemonDetail: React.FC = () => {
       .split("-")
       .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
       .join(separator);
+  }
+
+  if (loadingCard) {
+    return (
+      <div className="container mb-5" style={{ marginTop: "6em" }}>
+        <div className="card">
+          <div className="card-img-top d-flex justify-content-center">
+            <div
+              className="spinner-border text-primary"
+              role="status"
+              style={{ marginTop: "20px" }}
+            >
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+          <div className="card-body">
+            <h1 className="card-title">
+              <div className="placeholder-glow">
+                <span className="placeholder col-6"></span>
+              </div>
+            </h1>
+            <div className="card-text">
+              <h2>
+                <div className="placeholder-glow">
+                  <span className="placeholder col-4"></span>
+                </div>
+              </h2>
+              <ul>
+                {[...Array(5)].map((_, index) => (
+                  <li key={index}>
+                    <div className="placeholder-glow">
+                      <span className="placeholder col-12"></span>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+              <h2>
+                <div className="placeholder-glow">
+                  <span className="placeholder col-4"></span>
+                </div>
+              </h2>
+              <ul>
+                {[...Array(5)].map((_, index) => (
+                  <li key={index}>
+                    <div className="placeholder-glow">
+                      <span className="placeholder col-12"></span>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
